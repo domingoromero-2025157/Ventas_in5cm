7bc3a2789a0c527699f6c12ba748c2570e62a0ba
DomingoRomero2025157 <dromero-2025157@kinal.edu.gt>
2026-04-08 08:45:24 -0600

    fix: se han corregido errores

--- demo/src/main/java/com/Ventas_in5cm/demo/Controller/HomeController.java
+++ demo/src/main/java/com/Ventas_in5cm/demo/Controller/HomeController.java
@@ -5,13 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String inicio() {
-        return "home";
-    }
-
-    @GetMapping("/home")
-    public String home() { return "home"; }
 
     @GetMapping("/clientes")
     public String clientes() { return "cliente"; }
